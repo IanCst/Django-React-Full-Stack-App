cd03b4d52d44ba4cd92c10ecb178f17c2ef5ff69
zerol <szerinhu@gmail.com>
2024-10-11 13:44:35 -0300

    adding frontend basis

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '.App.jsx'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
