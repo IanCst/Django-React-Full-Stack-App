fccd713b1c43c3bebf5382cd0f11b7d6ce489166
zerol <szerinhu@gmail.com>
2024-10-11 23:19:41 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/login"
-import Register from "./pages/register"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
@@ -14,28 +14,26 @@ function Logout() {
 function RegisterAndLogout() {
   localStorage.clear()
   return <Register />
-  
 }
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/logout" element={<Logout />} />
-      <Route path="/register" element={<RegisterAndLogout />} />
-      <Route path="*" element={<NotFound />}></Route> 
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,40 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if(token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-    return config
-    },
-    (error) => {
-        return Promise.reject(error)
-    }
-)
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
-export default api
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
